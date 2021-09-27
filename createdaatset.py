8b680ba4b85969f726ebe5110be2dedf0a3ff69d
Adarsh Muthukumaran <adarshh2313@gmail.com>
2021-09-27 16:32:31 +0530

    Dataset changed

--- createdaatset.py
+++ createdaatset.py
@@ -4,8 +4,6 @@ import numpy as np
 import random
 random.seed(234)
 
-path = '/Users/Adarsh/Desktop/projects/cGAN-edge-generation/Dataset'
-
 img = []
 for _ in range(1000):
     img.append(np.random.randint(0,128,(8,8,1),np.uint8))
@@ -15,11 +13,11 @@ for _ in range(1000):
 for i in range(500):
     s = random.randint(2,6)
     img[i] = cv2.rectangle(img[i],(0,s),(8,8),(255),-1)
-    cv2.imwrite(os.path.join(path,'image{:03d}.png'.format(i+1)),img[i])
+    cv2.imwrite('/Users/Adarsh/Desktop/projects/cGAN-edge-generation/Dataset/horizontal/image{:03d}.png'.format(i+1),img[i])
 
 # For next 500 images, vertical edges:
 
 for i in range(500,1000):
     s = random.randint(2,6)
     img[i] = cv2.rectangle(img[i],(s,0),(8,8),(255),-1)
-    cv2.imwrite(os.path.join(path,'image{:03d}.png'.format(i+1)),img[i])
+    cv2.imwrite('/Users/Adarsh/Desktop/projects/cGAN-edge-generation/Dataset/vertical/image{:03d}.png'.format(i+1),img[i])
